56c6c0506900161125bca6f4684beb9315afdf32
Maxim Marchenko <slayer.jk@mail.ru>
2017-03-04 20:07:58 +0600

    Release - w3c checked; img compressed; minification done.

--- script/script.js
+++ script/script.js
@@ -3,12 +3,13 @@ $(document).ready(function () {
   'use strict';
   
   $.fn.slideSwitchingFunc = function () {
-    var $checkedInput = $('.guidlines__input:checked');
-    var $nextCheckedInput = $checkedInput.next();
-    var $currentSlide = $('.active-slide');
-    var $nextSlide = $currentSlide.next();
-    var $currentSwitch = $('.active-switch');
-    var $nextSwitch = $currentSwitch.next();
+    var
+      $checkedInput = $('.guidlines__input:checked'),
+      $nextCheckedInput = $checkedInput.next(),
+      $currentSlide = $('.active-slide'),
+      $nextSlide = $currentSlide.next(),
+      $currentSwitch = $('.active-switch'),
+      $nextSwitch = $currentSwitch.next();
     
     
     $checkedInput.removeAttr('checked');
@@ -21,58 +22,6 @@ $(document).ready(function () {
     }
   };
   
-  setInterval($.fn.slideSwitchingFunc, 1000);
+  setInterval($.fn.slideSwitchingFunc, 3000);
 
 });
-
-
-
-/*
-var main = function() {
-    var nextPageF = function () {
-        var currentSlide = $('.active-slide');
-        var nextSlide = currentSlide.next();
-        var currentDot = $('.active-dot');
-        var nextDot = currentDot.next();
-
-        if(nextSlide.length == 0) {
-            nextSlide = $('.slide').first();
-            nextDot = $('.dot').first();
-        }           
-
-        currentSlide.fadeOut(600).removeClass('active-slide');
-        nextSlide.fadeIn(600).addClass('active-slide');
-
-        currentDot.removeClass('active-dot');
-        nextDot.addClass('active-dot');
-    };
-
-    var prefPageF = function () {
-        var currentSlide = $('.active-slide');
-        var prevSlide = currentSlide.prev();
-        var currentDot = $('.active-dot');
-        var prevDot = currentDot.prev();
-
-        if(prevSlide.length == 0) {
-            prevSlide = $('.slide').last();
-            prevDot = $('.dot').last();
-        }
-
-        currentSlide.fadeOut(600).removeClass('active-slide');
-        prevSlide.fadeIn(600).addClass('active-slide');
-
-        currentDot.removeClass('active-dot');
-        prevDot.addClass('active-dot');
-    };
-
-    var interval = setInterval(nextPageF, 20000);
-    $('.arrow-next').click(function(){
-        clearInterval(interval );
-        nextPageF();
-    });
-    $('.arrow-prev').click(function(){
-        clearInterval(interval );
-        prefPageF();
-    });
-};
-*/
